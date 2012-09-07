03b2e9e557f8a23bf73e2ecf24328b28a0c9845e
Fahmi Abdulhamid <findfahmi@gmail.com>
2012-09-07 22:41:08 +1200

    Added test level.

--- player.js
+++ player.js
@@ -9,6 +9,12 @@ var Player = me.ObjectEntity.extend(
         // Set the default horizontal & vertical speed (accel vector)
         this.setVelocity(3, 3);
         this.gravity = 0; // 0 as this is a top-down, not a platformer
+
+        // Adjust the bounding box
+		this.updateColRect(5, 22, 5, 22);
+
+        // Set the display to follow our position on both axis
+	    me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     },
 
     update: function()
@@ -18,6 +24,9 @@ var Player = me.ObjectEntity.extend(
             this.parent(this);
             return true;
         }
+
+        var horzMovement = 1;
+        var vertMovement = 0;
 
         // Player movement
         if(me.input.isKeyPressed('left'))
@@ -37,6 +46,9 @@ var Player = me.ObjectEntity.extend(
             this.vel.x = 0;
             this.vel.y = 0;
         }
+
+        //this.vel.x = Math.cos(Math.PI/2 * horzMovement) * this.accel.x * me.timer.tick;
+        //this.vel.y = Math.sin(Math.Pi/2 * vertMovement) * this.accel.x * me.timer.tick;
 
         // Notify of player movement
         this.updateMovement();
