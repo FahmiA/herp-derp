f6bbff25d40b9e349828c1299d77cf0424b09f3d
Fahmi Abdulhamid <findfahmi@gmail.com>
2012-09-09 12:44:31 +1200

    Removed debug collision bounds.

--- main.js
+++ main.js
@@ -38,8 +38,8 @@ var g_resources= [
     // Level 1
     name: 'area01',
     type: 'tmx',
-    src: 'data/aniTest.tmx'
-    //src: 'data/testlevel.tmx'
+    //src: 'data/aniTest.tmx'
+    src: 'data/testlevel.tmx'
 }, {
     //Title screen image
     name: "title_screen",
@@ -73,7 +73,7 @@ var jsApp        =
         // Load everything & display a loading screen
         me.state.change(me.state.LOADING);
 
-        me.debug.renderHitBox = true;
+        //me.debug.renderHitBox = true;
     },
 
 
